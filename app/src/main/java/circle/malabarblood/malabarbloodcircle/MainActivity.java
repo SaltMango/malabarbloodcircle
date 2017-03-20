10a2169ec21bdc5c08e7e3e73cbeaa712593e5af
Sreenath Maniyeeri <mvsreenath@gmail.com>
2017-03-20 19:37:34 +0530

    add Donner

--- app/src/main/java/circle/malabarblood/malabarbloodcircle/MainActivity.java
+++ app/src/main/java/circle/malabarblood/malabarbloodcircle/MainActivity.java
@@ -1,5 +1,6 @@
 package circle.malabarblood.malabarbloodcircle;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,12 +20,16 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
+
+
+
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
             }
         });
     }
@@ -48,5 +54,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void addDonnerTest(View view) {
+
+        Intent intent = new Intent(this,AddUserActivity.class);
+        startActivity(intent);
+
     }
 }
